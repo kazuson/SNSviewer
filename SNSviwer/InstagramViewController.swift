1fafe8d6d80744602f1af4085aea96649ca64f02
kazuma kogami <kogatiii@gmail.com>
2016-02-11 21:30:14 +0900

    add-view-controller

---
+++ SNSviwer/InstagramViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InstagramViewController.swift
+//  SNSviwer
+//
+//  Created by 小紙一馬 on 2016/02/11.
+//  Copyright © 2016年 kazumakogami. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InstagramViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
