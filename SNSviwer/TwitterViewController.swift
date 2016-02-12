55bc94c205b2c62f6872848ae7d4eb11dfdfc6be
kazuma kogami <kogatiii@gmail.com>
2016-02-12 12:46:46 +0900

    add-twitter-view

--- SNSviwer/TwitterViewController.swift
+++ SNSviwer/TwitterViewController.swift
@@ -11,15 +11,41 @@ import UIKit
 
 class TwitterViewController: UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var reload: UIBarButtonItem!
+    @IBOutlet weak var stopButton: UIBarButtonItem!
+    
+    //twitterのurl起動時にこのページを開く
+    let homeUrl = "https://twitter.com/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        openUrl(homeUrl)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func openUrl(urlString: String){
+        let url = NSURL(string: urlString)
+        let urlRequest = NSURLRequest(URL: url!)
+        webView.loadRequest(urlRequest)
+    }
+    
+    @IBAction func backButtonTapped(sender: UIBarButtonItem) {
+        webView.goBack()
+    }
+    @IBAction func reloadButtonTapped(sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    @IBAction func stopButtonTapped(sender: UIBarButtonItem) {
+        webView.stopLoading()
+    }
+   
     
     
 }
