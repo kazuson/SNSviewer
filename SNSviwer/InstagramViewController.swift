8da5e8fda83f167d9c623eae1140f73abfb2b7ab
kazuma kogami <kogatiii@gmail.com>
2016-02-12 11:00:21 +0900

    add-instagram-view

--- SNSviwer/InstagramViewController.swift
+++ SNSviwer/InstagramViewController.swift
@@ -11,15 +11,44 @@ import UIKit
 
 class InstagramViewController: UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var reload: UIBarButtonItem!
+    @IBOutlet weak var stopButton: UIBarButtonItem!
+    
+    //instagramのurl起動時にこのページを開く
+    let homeUrl = "https://www.instagram.com/"
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        openUrl(homeUrl)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func openUrl(urlString: String){
+        let url = NSURL(string: urlString)
+        let urlRequest = NSURLRequest(URL: url!)
+        webView.loadRequest(urlRequest)
+    }
+    
+    @IBAction func backButtonTapped(sender: UIBarButtonItem) {
+        webView.goBack()
+    }
+    @IBAction func reloadButtonTapped(sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    @IBAction func stopButtonTapped(sender: UIBarButtonItem) {
+        webView.stopLoading()
+    }
+    
     
     
 }
